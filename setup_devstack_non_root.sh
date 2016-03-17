ad7c45c90c6f098c92de9af80a275cef31d3612b
Ryan McNair <mcnair@Ryans-MacBook-Pro.local>
2016-03-17 17:30:15 -0500

    Add cron job to remount LVM on reboot

--- setup_devstack_non_root.sh
+++ setup_devstack_non_root.sh
@@ -7,14 +7,26 @@ for key in `ls /vagrant/keys/`; do
     chmod 600 ~/.ssh/$key
 done
 
+# Cron was having trouble with file from shared folder
+cp /vagrant/remount_lvm_backing_file.sh ~/remount_lvm_backing_file.sh
+# Setup job to remount LVM backing file for devstack after boot
+(crontab -l 2>/dev/null; echo "@reboot ~/remount_lvm_backing_file.sh") | crontab -
+
+# Time to stack
 git clone https://git.openstack.org/openstack-dev/devstack
 cd devstack
-echo '[[local|localrc]]' > local.conf
-echo ADMIN_PASSWORD=password >> local.conf
-echo MYSQL_PASSWORD=password >> local.conf
-echo RABBIT_PASSWORD=password >> local.conf
-echo SERVICE_PASSWORD=password >> local.conf
-echo SERVICE_TOKEN=tokentoken >> local.conf
+# Change these passwords...
+pass=password
+token=tokentoken
+cat >local.conf <<EOL
+[[local|localrc]]
+ADMIN_PASSWORD=$pass
+MYSQL_PASSWORD=$pass
+RABBIT_PASSWORD=$pass
+SERVICE_PASSWORD=$pass
+SERVICE_TOKEN=$token
+EOL
+
 ./stack.sh
 
 /vagrant/config_git.sh
